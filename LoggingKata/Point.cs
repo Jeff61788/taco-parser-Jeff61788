c4ca7c7bc4e1a65ab9d22da5153b95e2c5d93dc2
jeff <jeffaycock3@gmail.com>
2020-02-20 18:42:47 -0600

    finished tacoParser Project

--- LoggingKata/Point.cs
+++ LoggingKata/Point.cs
@@ -1,7 +1,14 @@
-﻿namespace LoggingKata
+﻿
+namespace LoggingKata
 {
     public struct Point
     {
+
+        public Point(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
         public double Longitude { get; set; }
         public double Latitude { get; set; }
     }
